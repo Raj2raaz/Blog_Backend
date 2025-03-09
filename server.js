74a9456e00b608796ea2d65a055aa10be870058d
Subham Raj <rajshubham199@gmail.com>
2025-03-09 20:50:24 +0530

    added frontned link

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ dotenv.config();
 dbConnect();
 
 const app = express();
+
+const allowedOrigins = [
+  "http://localhost:5173", 
+  "https://blogi-subham.netlify.app/", 
+];
 const PORT = process.env.PORT || 4000;
 
 
@@ -23,7 +28,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"], 
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, 
